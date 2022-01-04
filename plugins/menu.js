63f69fd1ec51a18ed0e47973115fa4b3cddb438f
Samantha-a <95078987+Samantha-a@users.noreply.github.com>
2022-01-04 21:23:12 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,142 +1,31 @@
+/* Codded by @Ravindu Manoj
+
+Telegram: t.me/RavinduManoj
+Facebook: https://www.facebook.com/ravindu.manoj.79
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+Whats bot - Ravindu Manoj
+*/
 const Asena = require('../events');
 const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
 const axios = require('axios');
-
 const Language = require('../language');
 const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'menu', fromMe: false, desc: Lang.WP}, (async (message, match) => {
 
     var r_text = new Array ();
-    
-    
-    r_text[0] = "https://www.pixelstalk.net/wp-content/uploads/2016/03/Pikachu-wallpaper-HD-dowload.jpg";
-    
-    
+
+
+    r_text[0] = "https://ibb.co/kgs7J8Z";
+
+
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━━━━✨️Ameer Suhail✨️━━━━━⛦
-
-
-𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 𝐚 Ameer Suhail 𝐛𝐨𝐭.
-            *Pikachu*
-
-■□■□■□■□■□■□■□■□■□■□
-♕ *𝙈𝙚𝙙𝙞𝙖 𝘾𝙤𝙢𝙢𝙖𝙣𝙙𝙨* ♕
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -song <text>
-🍙𝙐𝙨𝙚➜ Downloads song for you.
-⚠️.song  baby love
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -video <yt link>
-🍙𝙐𝙨𝙚➜ Downloads video from YouTube link.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -insta <link>
-🍙𝙐𝙨𝙚➜   Downloads content from instagram.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -yt<text>
-🍙𝙐𝙨𝙚➜   Gives you YT links.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -show <show name>
-🍙𝙐𝙨𝙚➜   Get info related to tv series and shows.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -gif 
-🍙𝙐𝙨𝙚➜ Converts video to gif.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -tomp3 
-🍙𝙐𝙨𝙚➜ Converts video into audio.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -say <text>
-🍙𝙐𝙨𝙚➜ Converts text into voice.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -img <text>
-🍙𝙐𝙨𝙚➜ It sends image from google.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -trt <language code>
-🍙𝙐𝙨𝙚➜ Translate the text you tag.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -wiki <text>
-🍙𝙐𝙨𝙚➜ It sends Wikipedia result.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -lyric <text>
-🍙𝙐𝙨𝙚➜ Finds the lyrics of the song.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -covid <country code>
-🍙𝙐𝙨𝙚➜ Send the covid stats of your country.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -weather <city>
-🍙𝙐𝙨𝙚➜ Tells you about the weather of your place.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -removebg 
-🍙𝙐𝙨𝙚➜ Removes the background of tge image.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -ocr
-🍙𝙐𝙨𝙚➜ Finds the text written on the image.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -wallpaper
-🍙𝙐𝙨𝙚➜ It sends you random wallpaper.
-
-■□■□■□■□■□Pikachu■□■□■□■□■□
-♟ *𝙁𝙪𝙣 𝘾𝙤𝙢𝙢𝙖𝙣𝙙𝙨* ♟
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -joke 
-🍙𝙐𝙨𝙚➜ It sends a random joke.
- 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -meme <text>
-🍙𝙐𝙨𝙚➜ Cations the image into a meme.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -quote 
-🍙𝙐𝙨𝙚➜ It sends a random quote.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -ss <website link>
-🍙𝙐𝙨𝙚➜ It sends the screenshot of the website.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -changesay <text>
-🍙𝙐𝙨𝙚➜ Converts text into changesay meme image.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -trumpsay
-🍙𝙐𝙨𝙚➜ Convert text into Trump's tweet.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -compliment 
-🍙𝙐𝙨𝙚➜ Gives you a compliment.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -bitly <link>
-🍙𝙐𝙨𝙚➜   Shorten your link.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -dict 
-🍙𝙐𝙨𝙚➜   Dictionary [-dict en;anime]
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -zodiac <leo> 
-🍙𝙐𝙨𝙚➜   Tells you about your horoscope.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -qr <text>
-🍙𝙐𝙨𝙚➜   Converts text into qr code.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -movie 
-🍙𝙐𝙨𝙚➜  Gives you info about movie.
-⚠️movie master
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -anime <text>
-🍙𝙐𝙨𝙚➜  Gives you info about anime.
-⚠️anime pikachu
-■□■□■□■□■Pikachu□■□■□■□■□■□
-♝ *𝙎𝙩𝙞𝙘𝙠𝙚𝙧 𝘾𝙤𝙢𝙢𝙖𝙣𝙙𝙨* ♝
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -sticker 
-🍙𝙐𝙨𝙚➜ Converts img/gif into a sticker.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -photo 
-🍙𝙐𝙨𝙚➜ Converts sticker into image.
-
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -attp <text>
-🍙𝙐𝙨𝙚➜ Converts text into glowing sticker.
-⚠️ex  attp pikachu
-■□■□■□■□■□■□■□■□■□■□
-═════💢Pikachu💢═════
-▣▣▣▣▣▣▣▣▣ameersuhail▣▣▣▣▣▣▣▣▣▣▣
-
-`}) 
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: '⛦━━━━━✨️Albin✨️━━━━━⛦\n𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 𝐚 VIDHYAMWOL 𝐛𝐨𝐭.\n            *VIDHYAMWOL*\n■□■□■□■□■□■□■□■□■□■□\n♕ *𝙈𝙚𝙙𝙞𝙖 𝘾𝙤𝙢𝙢𝙖𝙣𝙙𝙨* ♕\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -song <text>\n👻𝙐𝙨𝙚➜ Downloads song for you.\n🤖.song  baby love\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -video <yt link>\n👻𝙐𝙨𝙚➜ Downloads video from YouTube link.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -insta <link>\n👻𝙐𝙨𝙚➜   Downloads content from instagram.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -yt<text>\n👻𝙐𝙨𝙚➜   Gives you YT links.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -show <show name>\n👻𝙐𝙨𝙚➜   Get info related to tv series and shows.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -gif \n👻𝙐𝙨𝙚➜ Converts video to gif.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -tomp3 \n👻𝙐𝙨𝙚➜ Converts video into audio.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -say <text>\n👻𝙐𝙨𝙚➜ Converts text into voice.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -img <text>\n👻𝙐𝙨𝙚➜ It sends image from google.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -trt <language code>\n👻𝙐𝙨𝙚➜ Translate the text you tag.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -wiki <text>\n👻𝙐𝙨𝙚➜ It sends Wikipedia result.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -lyric <text>\n👻𝙐𝙨𝙚➜ Finds the lyrics of the song.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -covid <country code>\n👻𝙐𝙨𝙚➜ Send the covid stats of your country.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -weather <city>\n👻𝙐𝙨𝙚➜ Tells you about the weather of your place.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -removebg \n👻𝙐𝙨𝙚➜ Removes the background of tge image.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -ocr\n👻𝙐𝙨𝙚➜ Finds the text written on the image.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -wallpaper\n👻𝙐𝙨𝙚➜ It sends you random wallpaper.\n■□■□■□■□■□Vidhya■□■□■□■□■□\n♟ *𝙁𝙪𝙣 𝘾𝙤𝙢𝙢𝙖𝙣𝙙𝙨* ♟\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -joke \n👻𝙐𝙨𝙚➜ It sends a random joke.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -meme <text>\n👻𝙐𝙨𝙚➜ Cations the image into a meme.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -quote \n👻𝙐𝙨𝙚➜ It sends a random quote.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -ss <website link>\n👻𝙐𝙨𝙚➜ It sends the screenshot of the website.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -changesay <text>\n👻𝙐𝙨𝙚➜ Converts text into changesay meme image.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -trumpsay\n👻𝙐𝙨𝙚➜ Convert text into Trumps tweet.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -compliment \n👻𝙐𝙨𝙚➜ Gives you a compliment.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -bitly <link>\n👻𝙐𝙨𝙚➜   Shorten your link.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -dict \n👻𝙐𝙨𝙚➜   Dictionary [-dict en;anime]\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -zodiac <leo> \n👻𝙐𝙨𝙚➜   Tells you about your horoscope.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -qr <text>\n👻𝙐𝙨𝙚➜   Converts text into qr code.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -movie \n👻𝙐𝙨𝙚➜  Gives you info about movie.\n🤖movie lusifer\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -anime <text>\n👻𝙐𝙨𝙚➜  Gives you info about anime.\n🤖anime Vidhya\n■□■□■□■□■Vidhya□■□■□■□■□■□\n♝ *𝙎𝙩𝙞𝙘𝙠𝙚𝙧 𝘾𝙤𝙢𝙢𝙖𝙣𝙙𝙨* ♝\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -sticker \n👻𝙐𝙨𝙚➜ Converts img/gif into a sticker.\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -photo\n👻𝙐𝙨𝙚➜ Converts sticker into image.\n\n🤡Command➡logo\n👻Use➡ GIVE COMMENDS OF SOME TEXT TO IMAGE FEATURES\n\n🤡COMMAND➡-pubg <text>\n🤖.pubg vidhya\n👻USE➡ Convert Text into pubg theamed logo\n\n🤡Command➡ -freefire <text>\n🤖.freefire Vidhya\n👻Use➡ Convert Text into FREE FIRE themed LOGO\n\n🤡Command ➡-vidhya <text>\n🤖.vidhya ichu\n👻Use➡ Converts text into glitch logo\n\n🤡Command➡ -owner\n👻Use➡ know About git owner\n\n🤡Command➡ -git\n👻Use➡ Get git link\n\n🤡𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -attp <text>\n👻𝙐𝙨𝙚➜ Converts text into glowing sticker.\n🤖ex  attp Vidhya\n■□■□■□■□■□■□■□■□■□■□\n═════💢Vidhyamwol💢═════\n▣▣▣▣▣▣▣▣▣Albin-manoj▣▣▣▣▣▣▣▣▣▣▣'}) 
 
 }));
